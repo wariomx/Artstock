d50855975d9250c7c16a8ffac5531460ec79fc31
Remco <4744907+RemcoMi@users.noreply.github.com>
2024-09-22 00:11:02 +0800

    Cleanup DynamicMint

--- frontend/src/app/components/DynamicMint.tsx
+++ frontend/src/app/components/DynamicMint.tsx
@@ -5,23 +5,7 @@ import {FC} from "react";
 // import {useSimulateContract} from "wagmi";
 
 export const DynamicMint: FC = () => {
-    // Todo simulate flow
-    // const result = useSimulateContract({
-    //     abi,
-    //     address: '0xdd9Fa9ddD68dd5aA023149Df488B4985ADC0e667',
-    //     functionName: 'mintArt',
-    //     args: [
-    //         "NameTest", // _name
-    //         "DescriptionTest",// _description
-    //         "ImageTest",// _image
-    //         100, // _price
-    //         "0xa2972322047F044B5889A3180D082111632E528F", // to
-    //         1004 // tokenId
-    //     ],
-    // })
-
     const { writeContract } = useWriteContract()
-
     const mintToken = async () => {
         try {
             const result = writeContract({
@@ -48,7 +32,7 @@ export const DynamicMint: FC = () => {
         <button
             onClick={mintToken}
         >
-            mintToken
+            Mint NFT of your artwork
         </button>
     )
 };
